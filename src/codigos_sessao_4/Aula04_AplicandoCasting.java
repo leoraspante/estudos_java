dcf888198382b74a091049c260e4152df5d1cc6f
leoraspante <leonardo.raspante@gmail.com>
2025-09-09 15:00:14 -0300

    Adiciona código básico utilizando o recurso Casting

---
+++ src/codigos_sessao_4/Aula04_AplicandoCasting.java
@@ -0,0 +1,26 @@
+package codigos_sessao_4;
+
+// Importação da biblioteca Locale.
+import java.util.Locale;
+
+public class Aula04_AplicandoCasting {
+
+	public static void main(String[] args) {
+		
+		// Definição das variáveis, sem atribuição de valores.
+		int a, b;
+		double resultado;
+		
+		// Definição posterior de valores nas variáveis.
+		a = 5;
+		b = 2;
+		
+		// Aplicação do Casting (Double).
+		resultado = (double) a / b;
+		
+		// Exibição do resultado.
+		System.out.println(resultado);
+	}
+	
+	
+}
