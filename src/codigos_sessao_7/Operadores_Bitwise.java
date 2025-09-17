75692993d80300ccd92fc6cd73fcb6ae14b36840
leoraspante <leonardo.raspante@gmail.com>
2025-09-17 17:04:48 -0300

    Adiciona códigos criados nos estudos da sessão 7

---
+++ src/codigos_sessao_7/Operadores_Bitwise.java
@@ -0,0 +1,19 @@
+// Exemplo prático de utilização dos operadores bitwise.
+
+package codigos_sessao_7;
+
+public class Operadores_Bitwise {
+
+	public static void main(String[] args) {
+		
+		 // Declaração de variáveis com valores de exemplo.
+		 int n1 = 89;
+		 int n2 = 60;
+		 
+		 // Mensagem de saída.
+		 System.out.println(n1 & n2); // Operador E.
+		 System.out.println(n1 | n2); // Operador OU.
+		 System.out.println(n1 ^ n2); // Operador OU-Exclusivo.
+	}
+
+}
